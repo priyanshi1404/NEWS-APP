73e2ff6feb3c88fc424873265a257450622457c9
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-21 17:09:09 +0530

    Delete newsapp directory

--- newsapp/scripts/utils/config.js
+++
@@ -1,3 +0,0 @@
-export const URL =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=fe60c13e822e48069ccc424e43265ddb";
-//export default URL :  this strategy is used when only one URL has to be exported; if more objects have to be exported then use the upper used strategy
