0939b3fd20f400ec70f15e5c7fc1868934a4de80
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-24 00:16:38 +0530

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
 export const URL =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=ee4ba018bce34b6e95e4111499dc0a56";
+  "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&apiKey=ee4ba018bce34b6e95e4111499dc0a56";
 //export default URL :  this strategy is used when only one URL has to be exported; if more objects have to be exported then use the upper used strategy
