bd5f583a52fb7f0ec9a74d847186a5d4b5e7c270
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-24 00:14:36 +0530

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
 export const URL =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=fe60c13e822e48069ccc424e43265ddb";
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=ee4ba018bce34b6e95e4111499dc0a56";
 //export default URL :  this strategy is used when only one URL has to be exported; if more objects have to be exported then use the upper used strategy
