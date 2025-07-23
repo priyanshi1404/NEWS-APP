7fae31d76e4f8fe9ff2f8fba32a54bf73211cc22
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-24 00:07:23 +0530

    Add files via upload

---
+++ utils/config.js
@@ -0,0 +1,3 @@
+export const URL =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=fe60c13e822e48069ccc424e43265ddb";
+//export default URL :  this strategy is used when only one URL has to be exported; if more objects have to be exported then use the upper used strategy
